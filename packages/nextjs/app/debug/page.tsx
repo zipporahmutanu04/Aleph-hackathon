89cea6930b981de9000508d8a56b5f18581997c0
clemente-xyz <clementeserranosutil@gmail.com>
2024-08-09 15:34:06 +0200

    styles: adapt contracts page to lisk branding

--- packages/nextjs/app/debug/page.tsx
+++ packages/nextjs/app/debug/page.tsx
@@ -9,19 +9,19 @@ export const metadata = getMetadata({
 
 const Debug: NextPage = () => {
   return (
-    <>
+    <section className="flex flex-col items-center gap-4 w-full">
       <DebugContracts />
-      <div className="text-center mt-8 bg-secondary p-10">
-        <h1 className="text-4xl my-0">Debug Contracts</h1>
-        <p className="text-neutral">
+      <div className="p-8 flex flex-col gap-4 items-center rounded-lg border border-[#16203E] bg-base-200">
+        <h1 className="text-2xl my-0">Debug Contracts</h1>
+        <p className="text-slate-400 text-center">
           You can debug & interact with your deployed contracts here.
           <br /> Check{" "}
-          <code className="italic bg-base-300 text-base font-bold [word-spacing:-0.5rem] px-1">
-            packages / nextjs / app / debug / page.tsx
+          <code className="italic bg-base-100 text-white p-1 rounded-md text-base font-bold max-w-full break-words break-all inline-block">
+            packages/nextjs/app/debug/page.tsx
           </code>{" "}
         </p>
       </div>
-    </>
+    </section>
   );
 };
 
--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { Logo } from "~~/components/Logo";
 import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
@@ -14,7 +15,9 @@ const Home: NextPage = () => {
       <div className="px-5 flex flex-col gap-2 items-center">
         <h1 className="text-center">
           <span className="block text-base mb-2">Welcome to</span>
-          <span className="block text-5xl font-bold">Scaffold-Lisk </span>
+          <span className="flex items-end gap-4 text-5xl font-bold">
+            <Logo size={48} /> Scaffold-Lisk{" "}
+          </span>
         </h1>
         <div className="flex btn btn-md bg-base-100 w-fit justify-center mb-4 items-center space-x-2">
           <p className="my-2 font-medium">Connected Address:</p>
@@ -41,7 +44,7 @@ const Home: NextPage = () => {
       <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
         <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
           <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-            <BugAntIcon className="h-8 w-8 fill-secondary" />
+            <BugAntIcon className="h-8 w-8" />
             <p>
               Tinker with your smart contract using the{" "}
               <Link href="/debug" passHref className="link">
@@ -51,7 +54,7 @@ const Home: NextPage = () => {
             </p>
           </div>
           <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-            <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+            <MagnifyingGlassIcon className="h-8 w-8" />
             <p>
               Explore your local transactions with the{" "}
               <Link href="/blockexplorer" passHref className="link">
